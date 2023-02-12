2155d6ba4c1d5bec44399d88a4bbd12976b73e75
M. A. Startsev <m.a.startsev@gmail.com>
2023-02-12 23:47:00 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryName = 'gallery';
+const galleryEl = document.querySelector(`.${galleryName}`);
+
+const createGallery = ({ gallery, galleryName }) =>
+  (gallery.innerHTML = galleryItems.reduce(
+    (arr, { preview, original, description }) =>
+      `${arr}\n
+        <a class='${galleryName}__item ${galleryName}__link' href='${original}'>
+            <img class='${galleryName}__image' src='${preview}' alt='${description}' width='320px'>
+        </a>
+    `,
+    ''
+  ));
+
+createGallery({ gallery: galleryEl, galleryName: galleryName });
+
+new SimpleLightbox('.gallery__item', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  fadeSpeed: 250,
+  scrollZoom: false,
+});
