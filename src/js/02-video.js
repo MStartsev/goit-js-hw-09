5f1cf0796062690da37bd8c3c6d1355a55656270
M. A. Startsev <m.a.startsev@gmail.com>
2023-02-13 19:18:25 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
-//
+import Player from '@vimeo/player';
+console.dir(Player);
+
+const player = new Player('vimeo-player');
+
+player.on('play', function () {
+  console.log('played the handstick video!');
+});
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+// //
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
