4e8e68fc8ff08f0786292e082eee11c6b5358ac8
M. A. Startsev <m.a.startsev@gmail.com>
2023-02-15 14:26:34 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,8 @@ import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
 const currentTimeKey = 'videoplayer-current-time';
-const currentTime = JSON.parse(localStorage.getItem(currentTimeKey)) || 0;
+const currentTime =
+  Number(JSON.parse(localStorage.getItem(currentTimeKey))) || 0;
 const throttleTime = 1000;
 
 player.setCurrentTime(currentTime);
