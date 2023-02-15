aa52e3840e22e4ceb0289e1ce90edd9c2c5965b0
M. A. Startsev <m.a.startsev@gmail.com>
2023-02-15 23:33:40 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const { email, message } = form;
+let storageValue = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+if (storageValue.email) email.value = storageValue.email;
+if (storageValue.message) message.value = storageValue.message;
+
+form.addEventListener('input', throttle(setStorage, 500));
+form.addEventListener('submit', submitForm);
+
+function setStorage(e) {
+  const targetEmail = e.target === email;
+  const targetMessage = e.target === message;
+  const value = e.target.value.trim();
+
+  if (!targetEmail && !targetMessage) return;
+  targetEmail ? (storageValue.email = value) : (storageValue.message = value);
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(storageValue));
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  if (!storageValue.email || !storageValue.message) return;
+
+  console.log(`${STORAGE_KEY}: `, storageValue);
+  storageValue = {};
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
