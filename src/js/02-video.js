292f10fae5bab4f44ee15d8d016b4e41c6073f4b
M. A. Startsev <m.a.startsev@gmail.com>
2023-02-18 02:45:47 +0200

    task corr

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,12 @@ import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
 const currentTimeKey = 'videoplayer-current-time';
-const currentTime =
-  Number(JSON.parse(localStorage.getItem(currentTimeKey))) || 0;
+const currentTime = Number(localStorage.getItem(currentTimeKey));
 const throttleTime = 1000;
 
 player.setCurrentTime(currentTime);
 player.on('timeupdate', throttle(onPlay, throttleTime));
 
 function onPlay(time) {
-  localStorage.setItem(currentTimeKey, JSON.stringify(time.seconds));
+  localStorage.setItem(currentTimeKey, time.seconds);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,31 +2,47 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-const { email, message } = form;
-let storageValue = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-
-if (storageValue.email) email.value = storageValue.email;
-if (storageValue.message) message.value = storageValue.message;
+let storageValue = {};
+getStorage(STORAGE_KEY);
 
 form.addEventListener('input', throttle(setStorage, 500));
 form.addEventListener('submit', submitForm);
 
+function getStorage(STORAGE_KEY) {
+  try {
+    const item = localStorage.getItem(STORAGE_KEY);
+    if (item) storageValue = JSON.parse(item);
+    writeValue(storageValue);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function writeValue(storageValue) {
+  Object.entries(storageValue).forEach(
+    ({ 0: key, 1: value }) => (form[key].value = value)
+  );
+}
+
 function setStorage(e) {
-  const targetEmail = e.target === email;
-  const targetMessage = e.target === message;
-  const value = e.target.value.trim();
-
-  if (!targetEmail && !targetMessage) return;
-  targetEmail ? (storageValue.email = value) : (storageValue.message = value);
-
+  storageValue[e.target.name] = e.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(storageValue));
 }
 
 function submitForm(e) {
   e.preventDefault();
-  if (!storageValue.email || !storageValue.message) return;
 
-  console.log(`${STORAGE_KEY}: `, storageValue);
+  const consoleMessage = [...form.querySelectorAll('[name]')].reduce(
+    (text, { name, value }) =>
+      !value
+        ? alert(`${name} field is empty!`)
+        : `${text}\n ${name} : ${value}`,
+    `${STORAGE_KEY}: `
+  );
+
+  if (!consoleMessage) return;
+
+  console.log(consoleMessage);
   storageValue = {};
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
